9236ba70b2edea0133938c9a1877da800f38f9e5
Austin Fonacier <afonacier@administrators-macbook-pro-7.local>
2010-01-08 14:12:03 -0800

    Hopefully fixing something

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,6 @@ class Question < ActiveRecord::Base
   validates_length_of :question, :minimum => 10
   
   # Find the undisabled questions within the last 5 minutes sorted by created_at
-  named_scope :ready_to_go, :conditions => ["disable = ? AND created_at > ? AND created_at <", false, 2.minutes.ago, 1.minute.ago], :limit => 50, :order => "created_at DESC"
+  named_scope :ready_to_go, :conditions => ["disable = ? AND created_at > ? AND created_at < ?", false, 2.minutes.ago, 1.minute.ago], :limit => 50, :order => "created_at DESC"
   named_scope :all_ready_to_go, :conditions => ["disable = ? AND created_at > ?", false, 1.minute.ago], :order => "created_at DESC"
 end
