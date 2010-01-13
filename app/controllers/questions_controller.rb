9f1d751cebc07634f124722791ed1cf00385a088
Austin Fonacier <afonacier@administrators-macbook-pro-7.local>
2010-01-12 17:33:48 -0800

    Not jsut for staging

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -120,11 +120,8 @@ class QuestionsController < ApplicationController
   
   private
     def authenticate
-      # only for staging
-      if RAILS_ENV == 'staging'
-        authenticate_or_request_with_http_basic do |user_name, password|
-          user_name == '72' && password == '72'
-        end
+      authenticate_or_request_with_http_basic do |user_name, password|
+        user_name == '72' && password == '72'
       end
     end
 end
